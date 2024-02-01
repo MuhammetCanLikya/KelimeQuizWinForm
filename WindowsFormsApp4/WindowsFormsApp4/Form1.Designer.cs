52d4f997be23c27fae478e2b2e9eb29f60e2b393
MuhammetCanLikya <07finikeli07143@gmail.com>
2024-02-01 03:29:29 +0300

    net472 calisan

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(96, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(342, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(216, 76);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(643, 395);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 43);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Sonraki Kelime";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(606, 127);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 43);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Kolay";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(606, 192);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 43);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Orta";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(606, 255);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 43);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Zor";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(284, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Kelimenin Zorluk Derecesi";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(606, 322);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 43);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Tanımlanmamış";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> words; // İngilizce kelimelerin ve Türkçe karşılıklarının saklandığı sözlük
+        private Random random;
+        private KeyValuePair<string, string> currentWordPair;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWords();
+            InitializeWordsWithAccessDatabase();
+            random = new Random();
+            DisplayRandomWord();
+        }
+
+        private void InitializeWords()
+        {
+            // Kelimeleri sözlüğe ekleyin (istediğiniz kadar kelime ekleyebilirsiniz)
+            words = new Dictionary<string, string>
+            {
+                
+            };
+        }
+
+        private void InitializeWordsWithAccessDatabase()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=kelimeler.accdb;Persist Security Info=False;";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT englishword, turkishword FROM tlbKelimeler"; // tblKelimeler, veritabanındaki tablonun adını temsil etmelidir.
+
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string englishWord = reader["englishword"].ToString();
+                            string turkishWord = reader["turkishword"].ToString();
+
+                            // Veritabanından alınan kelimeleri sözlüğe ekle
+                            if (!words.ContainsKey(englishWord))
+                            {
+                                words.Add(englishWord, turkishWord);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Hata durumunda işlemleriniz
+                    MessageBox.Show("Hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+
+            private void DisplayRandomWord()
+            {
+            // Rastgele bir kelime çekin
+            int randomIndex = random.Next(words.Count);
+            currentWordPair = words.ElementAt(randomIndex);
+
+            // İngilizce kelimeyi ekrana yazdırın
+            label1.Text = currentWordPair.Key;
+
+            // Butonlara rastgele kelimeleri ayarla
+            List<string> randomWords = GetRandomWords(currentWordPair.Value);
+            button1.Text = randomWords[0];
+            button2.Text = randomWords[1];
+            button3.Text = randomWords[2];
+            button4.Text = randomWords[3];
+            }
+
+
+        private List<string> GetRandomWords(string correctWord)
+        {
+            List<string> randomWords = new List<string> { correctWord };
+
+            // Diğer rastgele kelimeleri seçin (bu kısmı özelleştirebilirsiniz)
+            while (randomWords.Count < 4)
+            {
+                int randomIndex = random.Next(words.Count);
+                string randomWord = words.ElementAt(randomIndex).Value;
+
+                // Aynı kelimenin tekrar seçilmesini önleyin
+                if (!randomWords.Contains(randomWord))
+                {
+                    randomWords.Add(randomWord);
+                }
+            }
+
+            // Karıştırın
+            randomWords = randomWords.OrderBy(x => Guid.NewGuid()).ToList();
+
+            return randomWords;
+        }
+
+        public void wordCheck(string word)
+        {
+            string userAnswer = word;
+            string correctAnswer = currentWordPair.Value;
+            if (userAnswer == correctAnswer)
+            {
+                MessageBox.Show("Doğru!");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış. Doğru cevap: " + currentWordPair.Value);
+            }
+        }
+        public void difficultyCheck(string difficultyAnswer)
+        {
+            if (difficultyAnswer == "Kolay")
+            { }
+            else if (difficultyAnswer == "Orta")
+            { }
+            else if (difficultyAnswer == "Zor")
+            { }
+        }
+        public void UpdateDifficulty(string wordToUpdate, string newDifficulty)
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=kelimeler.accdb;Persist Security Info=False;";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // UPDATE SQL sorgusunu oluştur
+                    string updateQuery = "UPDATE tlbKelimeler SET difficulty = @newDifficulty WHERE englishword = @wordToUpdate";
+
+                    using (OleDbCommand updateCommand = new OleDbCommand(updateQuery, connection))
+                    {
+                        // Parametreleri ekleyin
+                        updateCommand.Parameters.AddWithValue("@newDifficulty", newDifficulty);
+                        updateCommand.Parameters.AddWithValue("@wordToUpdate", wordToUpdate);
+
+                        // SQL sorgusunu çalıştır
+                        int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Güncelleme başarılı.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Güncelleme yapılamadı. Belirtilen kelime bulunamadı.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wordCheck(button1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wordCheck(button2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            wordCheck(button4.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            wordCheck(button3.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DisplayRandomWord();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            UpdateDifficulty(label1.Text, "Kolay");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            UpdateDifficulty(label1.Text, "Orta");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            UpdateDifficulty(label1.Text, "Zor");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            UpdateDifficulty(label1.Text, "Tanımlanmamış");
+        }
+    }//class sonu
+}//namespace sonu
